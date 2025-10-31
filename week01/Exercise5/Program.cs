87eeae0fb76b42d1f8720be674e6787f4b2b3629
Lsonsprofile <badument@gmail.com>
2025-10-31 23:31:00 +0100

    finish week week exercise 5 program.cs

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,48 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+        // Call functions here
+        DisplayWelcome();
+
+        string userName = PromptUserName();
+        int number = PromptUserNumber();
+        int square = SquareNumber(number);
+
+        Console.WriteLine($"{userName}, the square of your number is {square}.");
+
+
+    }
+
+    // FUNCTIONS GO HERE (outside Main)
+
+    static void DisplayWelcome()
+    {
+        Console.WriteLine("**Welcome to the program!**");
+    }
+    static string PromptUserName()
+    {
+        Console.Write("Please enter your name: ");
+        string userName = Console.ReadLine();
+        return userName;
+    }
+
+    static int PromptUserNumber()
+    {
+        Console.Write("Please enter a number: ");
+        int userNumber = int.Parse(Console.ReadLine());
+        return userNumber;
+    }
+
+
+    static int SquareNumber(int userNumber)
+    {
+        int square = userNumber * userNumber;
+        return square;
+    }
+
+
+    static void DisplayResult(string userName, int userNumber)
+    {
+        Console.WriteLine($"{userName}, the square of is {SquareNumber(userNumber)}");
     }
 }
