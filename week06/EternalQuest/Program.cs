2def8b451d8de2b24c0e8d4d6a45939efdc6b6f1
Lsonsprofile <badument@gmail.com>
2025-12-06 23:08:32 +0100

    finish week 6 Eternal Quest Program

--- week06/EternalQuest/Program.cs
+++ week06/EternalQuest/Program.cs
@@ -2,7 +2,7 @@
 CREATIVITY & EXCEEDING REQUIREMENTS:
 - Eternal goals show "∞" symbol and a special message every time
 - Checklist goals show live progress and big bonus celebration
-- Full error handling on invalid menu choices and file loading
+- Full error handling on invalid menu choices and file loading for every input type
 - Clean, professional code structure that’s easy to extend with new goal types
 */
 using System;
